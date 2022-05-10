0aa2d4b7f2a0f92cfb12a8cc4d9ed8848cdf0f5d
Devpe007 <anddev007pe@gmail.com>
2022-05-10 00:41:33 -0300

    create the structure of the InfoSection component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+
 import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+
+import {
+  homeObjOne,
+  homeObjThree,
+  homeObjTwo,
+} from '../components/InfoSection/Data';
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,6 +30,10 @@ function Home() {
       />
 
       <HeroSection />
+
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
     </>
   );
 };
